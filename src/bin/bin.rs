b782e28efd4ab611bafe86a8be92cdf05e00e8db
imaandrew <14946103+imaandrew@users.noreply.github.com>
2022-08-22 14:07:44 -0400

    Add argument parsing

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -1,6 +1,24 @@
-fn main() {
-    match mipsasm::parser::parse(&std::fs::read_to_string("a").unwrap()) {
+use clap::{arg, command, value_parser};
+use std::error;
+use std::fs;
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let matches = command!()
+        .arg(
+            arg!([FILE] "File to assemble")
+                .required(true)
+                .value_parser(value_parser!(PathBuf)),
+        )
+        .get_matches();
+
+    let data: String = match matches.get_one::<PathBuf>("FILE") {
+        Some(file) => fs::read_to_string(file)?.parse()?,
+        None => panic!(),
+    };
+    match mipsasm::parser::parse(&data) {
         Ok(n) => println!("{:#?}", n),
         Err(e) => println!("Error: {}", e),
     }
+    Ok(())
 }
