8fc1ef0d5a8af6b0b64853e2fee602d9c94fe989
imaandrew <14946103+imaandrew@users.noreply.github.com>
2022-09-05 00:23:36 -0400

    Improve cli

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -1,31 +1,45 @@
-use clap::{arg, command, value_parser};
+use clap::{Parser, ValueEnum};
 use std::collections::HashMap;
 use std::error;
 use std::fs;
+use std::fs::File;
+use std::io::Write;
 use std::path::PathBuf;
 
-fn main() -> Result<(), Box<dyn error::Error>> {
-    let matches = command!()
-        .arg(
-            arg!([FILE] "File to assemble")
-                .required(true)
-                .value_parser(value_parser!(PathBuf)),
-        )
-        .arg(
-            arg!([SYMS] "List of symbols")
-                .required(false)
-                .value_parser(value_parser!(PathBuf)),
-        )
-        .get_matches();
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// Assemble or disassemble the input file
+    #[clap(arg_enum, value_parser)]
+    mode: Mode,
+    /// Write output to this file
+    #[clap(short, value_parser, value_name = "output")]
+    output_file: Option<PathBuf>,
+    /// Import symbols from this file
+    #[clap(short, value_parser, value_name = "syms")]
+    syms: Option<PathBuf>,
+    /// Use this file as input
+    #[clap(value_parser)]
+    input_file: PathBuf,
+    /// Use this address as the base address of the program
+    #[clap(default_value_t = String::from("0x80000000"), short, value_parser, value_name = "base addr")]
+    base_addr: String,
+}
 
-    let data: String = match matches.get_one::<PathBuf>("FILE") {
-        Some(file) => fs::read_to_string(file)?.parse()?,
-        None => panic!(),
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Mode {
+    Asm,
+    Disasm,
+}
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let cli = Cli::parse();
+
+    let syms: String = match cli.syms.as_deref() {
+        Some(syms) => fs::read_to_string(syms)?.parse()?,
+        None => String::new(),
     };
-    let syms: String = match matches.get_one::<PathBuf>("SYMS") {
-        Some(file) => fs::read_to_string(file)?.parse()?,
-        None => panic!(),
-    };
+
     let symbols: HashMap<String, u32> = HashMap::from_iter(syms.lines().map(|s| {
         let mut parts = s.split('=');
         let name = parts.next().unwrap().trim();
@@ -33,14 +47,62 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         let value = u32::from_str_radix(value.replace("0x", "").trim(), 16).unwrap();
         (name.to_string(), value)
     }));
-    let x = match mipsasm::parser::scan(&data, 0x800F52E0, symbols) {
-        Ok(n) => n,
-        Err(e) => panic!("Error: {}", e),
-    };
-    println!("{:#?}", x);
-    let a = mipsasm::assembler::assemble(x);
-    for i in a {
-        println!("{:08X}", i);
+
+    let addr = cli.base_addr.replace("0x", "");
+    let addr = u32::from_str_radix(&addr, 16)
+        .unwrap_or_else(|_| {
+            eprintln!("Error: Invalid base address `{}`", cli.base_addr);
+            std::process::exit(1);
+        });
+
+    match cli.mode {
+        Mode::Asm => {
+            let data: String = fs::read_to_string(cli.input_file)?.parse()?;
+            let output = match mipsasm::parser::scan(&data, addr, symbols) {
+                Ok(output) => output,
+                Err(e) => {
+                    eprintln!("Error: {}", e);
+                    std::process::exit(1);
+                },
+            };
+
+            let output = mipsasm::assembler::assemble(output);
+
+            if let Some(output_file) = cli.output_file {
+                let mut bytes = vec![];
+                for word in output {
+                    bytes.append(&mut word.to_be_bytes().to_vec());
+                }
+                File::create(output_file)?.write_all(&bytes)?;
+            } else {
+                println!("{:08X?}", output);
+            }
+        }
+        Mode::Disasm => {
+            let mut words = vec![];
+            let mut bytes = fs::read(cli.input_file)?;
+            loop {
+                let mut word = [0; 4];
+                word.copy_from_slice(&bytes[0..4]);
+                words.push(u32::from_be_bytes(word));
+                bytes.drain(0..4);
+                if bytes.is_empty() {
+                    break;
+                }
+            }
+            let output = mipsasm::disassembler::disassemble(words);
+
+            if let Some(output_file) = cli.output_file {
+                let mut f = File::create(output_file)?;
+                for inst in output {
+                    write!(f, "{}", inst)?;
+                }
+            } else {
+                for inst in output {
+                    println!("{}", inst);
+                }
+            }
+        }
     }
     Ok(())
 }
