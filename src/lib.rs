fb20fba397f50e3de8403cff022d9c5407345267
imaandrew <14946103+imaandrew@users.noreply.github.com>
2022-09-27 17:19:13 -0400

    Remove unneeded test

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,3 @@ pub mod assembler;
 mod ast;
 pub mod disassembler;
 pub mod parser;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
